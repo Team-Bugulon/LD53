0b1124f707b3c8ba648e98e1ef27b204f7cd65fb
Oroshibu <oroshibu32@gmail.com>
2023-04-30 18:37:40 +0200

    blourpus

---
+++ Assets/WallAlts.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class WallAlts : MonoBehaviour
+{
+    [SerializeField] string wallSpritesheetPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var objects = UnityEditor.AssetDatabase.LoadAllAssetsAtPath(wallSpritesheetPath);
+        var wallAlts = objects.Where(q => q is Sprite).Cast<Sprite>().ToList();
+        
+        if (Random.Range(0,5) > 0){
+            GetComponentsInChildren<SpriteRenderer>()[1].sprite = wallAlts[1];
+        } else
+        {
+            GetComponentsInChildren<SpriteRenderer>()[1].sprite = wallAlts[Random.Range(2,5)];
+        }
+    }
+}
