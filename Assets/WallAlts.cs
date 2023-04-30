32fea69f59b38eb5da9e6263a3fb64a3dee698a1
Oroshibu <oroshibu32@gmail.com>
2023-04-30 20:40:38 +0200

    fix stuff

--- Assets/WallAlts.cs
+++ Assets/WallAlts.cs
@@ -11,7 +11,9 @@ public class WallAlts : MonoBehaviour
     {
         var objects = UnityEditor.AssetDatabase.LoadAllAssetsAtPath(wallSpritesheetPath);
         var wallAlts = objects.Where(q => q is Sprite).Cast<Sprite>().ToList();
-        
+        //order them by name
+        wallAlts = wallAlts.OrderBy(q => q.name).ToList();
+
         if (Random.Range(0,5) > 0){
             GetComponentsInChildren<SpriteRenderer>()[1].sprite = wallAlts[1];
         } else
