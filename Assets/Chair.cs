9da5073ca01fdc497ae70351e500e62391ff6d92
Oroshibu <oroshibu32@gmail.com>
2023-05-01 06:18:51 +0200

    chair flips!!!!!!!

--- Assets/Chair.cs
+++ Assets/Chair.cs
@@ -18,5 +18,25 @@ public class Chair : MonoBehaviour
             customer.wave = wave;
             customer.Init();
         }
+
+        Table[] tables = GameObject.FindObjectsOfType<Table>();
+        //calculate distance to each table
+        float[] distances = new float[tables.Length];
+        for (int i = 0; i < tables.Length; i++)
+        {
+            distances[i] = Vector2.Distance(transform.position, tables[i].transform.position);
+        }
+
+        //find the closest table
+        float minDistance = Mathf.Min(distances);
+        int minIndex = System.Array.IndexOf(distances, minDistance);
+
+        var assignedTable = tables[minIndex];
+
+        //is he on the left or right side of the table
+        if (transform.position.x > assignedTable.transform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
     }
 }
